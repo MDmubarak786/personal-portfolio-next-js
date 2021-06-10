99959ec25a61337b7c0f9c3776376cdb78bb166c
Mohammed Mubarak <mohammedmubarakmk@gmail.com>
2021-06-10 23:04:53 +0530

    tiny the header part

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -3,27 +3,19 @@ import React from "react";
 import styles from "../../styles/header.module.css";
 export default function Header() {
   return (
-    <div>
-      <div className={styles.header}>
-        <div>
-          <h1 className={styles.headingName}>
-            MK<span>.</span>
-          </h1>
-        </div>
-        <div>
-          <div>
-            <ul className={styles.navlist}>
-              <li>Home</li>
-              <li>About</li>
-              <li>Resume</li>
-              <li>Projects</li>
-            </ul>
-          </div>
-        </div>
-        <div>
-          <button className={styles.contact} onClick={()=>console.log('Contact')}>Contact</button>
-        </div>
-      </div>
+    <div className={styles.header}>
+      <h1 className={styles.headingName}>
+        MK<span>.</span>
+      </h1>
+      <ul className={styles.navlist}>
+        <li>Home</li>
+        <li>About</li>
+        <li>Resume</li>
+        <li>Projects</li>
+      </ul>
+      <button className={styles.contact} onClick={() => console.log("Contact")}>
+        Contact
+      </button>
     </div>
   );
 }
