bf28f8898d0211ac4aea2b8a95a1fa4b628ca1ee
Mohammed Mubarak <mohammedmubarakmk@gmail.com>
2021-06-12 10:17:23 +0530

    new ...

--- components/Landing.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import styles from "../styles/landing.module.css";
-export default function Landing() {
-  return (
-    <div className={styles.landing}>
-      <h1>I'm Mubarak</h1>
-    </div>
-  );
-}
