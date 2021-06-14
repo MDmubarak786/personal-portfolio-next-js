20733557462f6a896b4cf01ba95a800c448599e1
Mohammed Mubarak <mohammedmubarakmk@gmail.com>
2021-06-14 12:41:39 +0530

    footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ import styles from "../styles/blobanimi.module.css";
 export default function Footer() {
   return (
     <div className={styles.footer}>
-      <h1 >
+      <h1>
         do you like this style ,"center this part \& we will design this part
         later"
       </h1>
@@ -30,16 +30,16 @@ export default function Footer() {
         >
           <path
             stroke="#121417"
-            stroke-linecap="square"
-            stroke-linejoin="bevel"
-            stroke-width="200"
+            strokeLinecap="square"
+            strokeLinejoin="bevel"
+            strokeWidth="200"
             d="M100 356l43.049-217 43.049 237 43.049-339 43.049 169 43.048-29 43.049 138 43.049-257 43.049 228 43.049-140 43.049-17 43.049 121 43.049 70 43.049-263 43.048 114 43.049 210 44.534-312 43.049 141 43.048 140 43.049-166 40.817-88 43.05 48 45.28 219 43.05-319 43.05 253 43.05-163 43.05 13 43.04 15 43.05 154 43.05-61 43.05 158L1436 92"
           ></path>
           <path
             stroke=" #6147C6"
-            stroke-linecap="square"
-            stroke-linejoin="bevel"
-            stroke-width="130"
+            strokeLinecap="square"
+            strokeLinejoin="bevel"
+            strokeWidth="130"
             d="M100 356l43.049-217 43.049 237 43.049-339 43.049 169 43.048-29 43.049 138 43.049-257 43.049 228 43.049-140 43.049-17 43.049 121 43.049 70 43.049-263 43.048 114 43.049 210 44.534-312 43.049 141 43.048 140 43.049-166 40.817-88 43.05 48 45.28 219 43.05-319 43.05 253 43.05-163 43.05 13 43.04 15 43.05 154 43.05-61 43.05 158L1436 92"
           ></path>
         </svg>
