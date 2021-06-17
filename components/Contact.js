10352845b9875792e91fb5e59944865103e7668e
Mohammed Mubarak <mohammedmubarakmk@gmail.com>
2021-06-17 19:05:16 +0530

    new added contacts perferct now

--- components/Contact.js
+++ components/Contact.js
@@ -1,47 +1,40 @@
 import Image from "next/image";
 import styles from "../styles/contact.module.css";
+import {
+  FaGithub,
+  FaInstagram,
+  FaLinkedin,
+  FaTwitter,
+  FaYoutube,
+} from "react-icons/fa";
 const Contact = () => {
+  const instagram = "https://www.instagram.com/scooby_doo.mk/";
+  const twitter = "https://twitter.com/MMubarakoo7";
+  const github = "https://github.com/MDmubarak786";
+  const linkedin = "https://www.linkedin.com/in/mohammed-mubarak/";
+  const youtube = "https://www.youtube.com/channel/UCS2LnbR7nR1ylerD7TZYrlQ/";
   return (
     <div id="contact" className={styles.contact}>
       <div className={styles.main}>
         <div className={styles.left}>
           <h2 className={styles.contactHead}>Contact</h2>
-          <ul>
-            <li>
-              <a
-                href="https://www.instagram.com/scooby_doo.mk/"
-                target="_blank"
-              >
-                <Image src="/instagram.svg" alt="html" height={60} width={60} />
-              </a>
-            </li>
-            <li>
-              <a href="https://twitter.com/MMubarakoo7" target="_blank">
-                <Image src="/twitter.svg" alt="html" height={60} width={60} />
-              </a>
-            </li>
-            <li>
-              <a href="https://github.com/MDmubarak786" target="_blank">
-                <Image src="/github.svg" alt="html" height={60} width={60} />
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.linkedin.com/in/mohammed-mubarak/"
-                target="_blank"
-              >
-                <Image src="/linkedin.svg" alt="html" height={60} width={60} />
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.youtube.com/channel/UCS2LnbR7nR1ylerD7TZYrlQ"
-                target="_blank"
-              >
-                <Image src="/youtube.svg" alt="html" height={77} width={77} />
-              </a>
-            </li>
-          </ul>
+          <div className={styles.iconList}>
+            <a href={github} target="_blank">
+              <FaGithub />
+            </a>
+            <a href={linkedin} target="_blank">
+              <FaLinkedin />
+            </a>
+            <a href={instagram} target="_blank">
+              <FaInstagram />
+            </a>
+            <a href={twitter} target="_blank">
+              <FaTwitter />
+            </a>
+            <a href={youtube} target="_blank">
+              <FaYoutube />
+            </a>
+          </div>
           <div>
             <h2 className={styles.email}>Email</h2>
             <p className={styles.emailadd}>mohammedmubarakmk@gmail.com</p>
@@ -53,9 +46,13 @@ const Contact = () => {
         </div>
 
         <div className={styles.right}>
-    
-            <Image src="/side.svg" alt="html" height={650} width={650} />
-     
+          <Image
+            src="/side.svg"
+            alt="html"
+            height={650}
+            width={650}
+            draggable="false"
+          />
         </div>
       </div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,26 @@ import Footer from "../components/Footer";
 import Home from "../components/Home";
 import Skills from "../components/Skills";
 import Contact from "../components/Contact";
+import "font-awesome/css/font-awesome.min.css";
 export default function Index() {
   return (
     <>
       <Head>
         <title>Mohammed Mubarak | Personal Portfolio</title>
         <meta name="description" content="Mohammed Mubarak" />
+        <meta name="theme-color" content="#9d4edd" />
+
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Akronim&family=Paprika&display=swap"
           rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+          integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
+          crossOrigin="anonymous"
         />
       </Head>
       <Home />
